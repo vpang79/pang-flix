ceee733f972a274c0a6c1ad8738fbd448cc2a9e4
Vincent Pang <vpang23@gmail.com>
2019-09-26 02:53:47 +0000

    courses listing in index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+  resources :courses, only: [:index, :show]
   namespace :instructor do
     resources :courses, only: [:new, :create, :show]
   end
+
 end
