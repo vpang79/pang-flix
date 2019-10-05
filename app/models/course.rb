e49ca1fdb1233a9b876c5b0c9ecbef2f611c74e9
Vincent Pang <vpang23@gmail.com>
2019-10-05 05:25:38 +0000

    user enrollment completed

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,8 +1,11 @@
 class Course < ApplicationRecord
+  mount_uploader :image, ImageUploader
+
   belongs_to :user
   has_many :sections
+  has_many :enrollments
 
-  mount_uploader :image, ImageUploader
+  
 
   validates :title, presence: true
   validates :description, presence: true
